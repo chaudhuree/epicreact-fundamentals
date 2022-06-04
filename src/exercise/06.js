7a74966c0aa77077c03c4eeb484f5b3e31dbcfb6
chaudhuree <chaudhuree00@gmail.com>
2022-06-04 22:26:39 +0600

    docs: form basics

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -9,30 +9,32 @@ function UsernameForm({onSubmitUsername}) {
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
-
   // 🐨 add the onSubmit handler to the <form> below
-
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handleSubmit(event) {
+    event.preventDefault()
+    const value = event.target.elements.username.value
+    // const inputvalue = document.querySelector('#username').value
+    // console.log(inputvalue)
+    onSubmitUsername(value)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="user-name" id="username" />
       </div>
       <button type="submit">Submit</button>
     </form>
   )
 }
-
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
   return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
-
 export default App
